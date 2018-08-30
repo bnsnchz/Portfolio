b1105134e41decc0689877ec02061be8c3a6fdeb
bnsnchz <bsanchez4488@gmail.com>
2018-08-30 14:20:19 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import './animate.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="title">Hi.</h1>
+      <div className="main">
+        <div className="intro">
+          <h1 className="title animated fadeInUp">Hi.</h1>
+          <h2 className="name animated fadeInUpBig">I'm Ben</h2>
+        </div>
+  
+        <div className="bio">
+          <img className="logo" src="./assets/images/logo.png" alt="logo" />
+          <p>I'm a MERN stack developer based out of Los Angeles, CA. A passionate, problem solving web developer who values the listening aspect of communication as much as conversing. Loves design because of the opportunity to dissect items and reassemble for everyone's benefit. Lifetime learner and ready to help others make something great.</p>
+        </div>
+      </div>
+
+
       </div>
     );
   }
