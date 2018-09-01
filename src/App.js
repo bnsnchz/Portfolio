67e19bbc15eacba10299afde9f128f903b4816ea
bnsnchz <bsanchez4488@gmail.com>
2018-08-31 22:10:33 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import './animate.css'
+import Background from "./wp.gif"
+
+var wallPaper = {
+  backgroundPosition:"center top",
+  backgroundImage:`url(${Background})`,
+  backgroundRepeat:"no-repeat"
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="main">
-          <div className="intro">
+          <div className="intro"style={wallPaper}>
             <h1 className="title animated fadeInUp">Hi.</h1>
             <h2 className="name animated fadeInUpBig">I'm Ben</h2>
           </div>
-          <img className="wp" src="./assets/images/wp.gif" alt="wallpaper" />
           <div className="bio">
             <img className="logo" src="./assets/images/logo.png" alt="logo" />
             <p className="bioText">I'm a MERN stack developer based out of Los Angeles, CA. A passionate, problem solving web developer who values the listening aspect of communication as much as conversing. Loves design because of the opportunity to dissect items and reassemble for everyone's benefit. Lifetime learner and ready to help others make something great.</p>
