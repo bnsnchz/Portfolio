f7043c26115b3eb00423d90214d4b1346fba84fb
bnsnchz <bsanchez4488@gmail.com>
2018-09-01 23:48:11 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ var wallPaper = {
 }
 
 class App extends Component {
+
+componentDidMount(){
+  window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+  }
+}
+
   render() {
     return (
       <div className="App">
@@ -23,6 +30,17 @@ class App extends Component {
             <p className="bioText">I'm a MERN stack developer based out of Los Angeles, CA. A passionate, problem solving web developer who values the listening aspect of communication as much as conversing. Loves design because of the opportunity to dissect items and reassemble for everyone's benefit. Lifetime learner and ready to help others make something great.</p>
           </div>
         </div>
+        <footer className="footer">
+          <p className="footer1">2018 Ben Sanchez</p>
+          <br/>
+          <p className="footer2">Connect With Me:</p>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/bnsnchz/"><img className="linkedIn" src="./assets/images/LinkedIn.png" alt="LinkedIn"/></a>
+            <a href="https://github.com/bnsnchz"><img className="gitHub" src="./assets/images/GitHub.png" alt="Github"/></a>
+            <a href="mailto:bnsnchzdev@gmail.com?Subject=Job%20Inquiry"><img className="mail" src="./assets/images/Mail.png" alt="mail" /></a>
+          </div>
+
+        </footer>
       </div>
     );
   }
