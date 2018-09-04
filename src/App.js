1189e7f4b3eb7c578b70d75d774d6cd2d0bd967a
bnsnchz <bsanchez4488@gmail.com>
2018-09-04 01:24:37 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import './App.css';
 import './animate.css'
 import Background from "./wp.gif"
 
+/* eslint-disable */
+
 var wallPaper = {
-  backgroundPosition:"center top",
-  backgroundImage:`url(${Background})`,
-  backgroundRepeat:"no-repeat"
+  backgroundPosition: "center top",
+  backgroundImage: `url(${Background})`,
+  backgroundRepeat: "no-repeat"
 }
 
 class App extends Component {
 
-componentDidMount(){
-  window.onbeforeunload = () => {
-  window.scrollTo(0, 0);
+  componentDidMount() {
+    window.onbeforeunload = () => {
+      window.scrollTo(0, 0);
+    }
   }
-}
 
   render() {
     return (
       <div className="App">
         <div className="main">
-          <div className="intro"style={wallPaper}>
+          <div className="intro" style={wallPaper}>
             <h1 className="title animated fadeInDown">Hi.</h1>
             <h2 className="name animated fadeInDownBig">I'm Ben</h2>
           </div>
@@ -29,15 +31,69 @@ componentDidMount(){
             <img className="logo" src="./assets/images/logo.png" alt="logo" />
             <p className="bioText">I'm a MERN stack developer based out of Los Angeles, CA. A passionate, problem solving web developer who values the listening aspect of communication as much as conversing. Loves design because of the opportunity to dissect items and reassemble for everyone's benefit. Lifetime learner and ready to help others make something great.</p>
           </div>
+          <div className="modal fade" id="resume" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <h3 className="resumeName">Ben Sanchez</h3>
+                  <p className="resumeInfo"><a href="mailto:bnsnchzdev@gmail.com?Subject=Job%20Inquiry">bnsnchzdev@gmail.com</a> | 951-990-2607 | Los Angeles, California</p>
+                  <p className="resumeLinks"><a href="https://www.linkedin.com/in/bnsnchz/" target="_blank" >LinkedIn</a> | <a href="https://github.com/bnsnchz" target="_blank" >GitHub</a></p>
+
+                  <p><b><u>OBJECTIVE</u></b></p>
+                  <p>Entry-level full-time position in web development, with a special interest in Front-end React.js development.</p> 
+                  <p><b><u>TECHNICAL SKILLS</u></b></p>
+                  <p>React.js, Node.Js, Express, JavaScript, jQuery, React Native, GIT, Github, MongoDB, MySQL, Firebase, HTML, CSS, Bootstrap, Media Queries, APIs, JSON, REST, AJAX, the command line, computer science fundamentals, writing tests.</p>
+                  <p><b><u>PROJECTS</u></b></p>
+                  <ul>
+                    <p><b><u>Ethr</u></b><br/>
+                    <a href="https://ethr.herokuapp.com" target="_blank"><u>https://ethr.herokuapp.com</u></a> <a href="https://github.com/bnsnchz/ethr" target="_blank"><u>https://github.com/bnsnchz/ethr</u></a> </p> 
+                  <li>Review website centered on video games, allowing a user to create an account and post reviews.</li> 
+                  <li>Main front end JS, and Handlebars and MYSQL developer in group of 4 on 1-week deadline.</li>
+                  <li>Built MYSQL database to save user data and review posts.</li>
+                  </ul>
+                  <ul>
+                    <p><b><u>Look & Cook</u></b><br/>
+                    <a href="https://lookncook.herokuapp.com" target="_blank">https://lookncook.herokuapp.com</a>  <a href="https://github.com/bnsnchz/lookncook" target="_blank">https://github.com/bnsnchz/lookncook</a>
+                    </p>
+                  <li>React based interactive recipe website.</li>
+                  <li>Lead developer in group of 4 working in full MERN stack app on 1-week deadline.</li>
+                  <li>Created and maintained React components and Mongo Database.</li>
+                  <li>In charge of all CSS and REGEX formulas for timer and form submit functionality.</li>
+                  </ul>
+                  <p><b>EXPERIENCE</b></p> 
+                  <p><b><u>Lifetouch National School Studios</u></b></p>                                          <p>6/2011-4/2018</p>
+                  <ul>
+                    <p><b><u>Studio Lead/Photographer</u></b></p>
+                  <li>Worked as a photographer and studio manager for the flagship studio.</li>
+                  <li>Trained and oversaw new hires and executed daily quality checks.</li>
+                  <li>Led studio to receive Platinum Certification two years in a row, seeing 100+ sitting increase annually.</li>
+                  </ul>
+                  <p><b><u>EDUCATION</u></b></p> 
+                    <p><b><u>University of California Los Angeles</u></b></p>                                     <p>5/2018-8/2018</p>
+                  <p><b>UCLA Boot Camp</b></p> 
+                  <p>An intensive 12-week long boot camp dedicated to designing and building web applications. Skills learned consisted of HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handlebars JS, & React Js. 
+                  </p>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="Projects">
+            <h2 className="projectTitle"><u>These are some of the projects I've contributed to:</u></h2>
+
+          </div>
         </div>
         <footer className="footer">
           <p className="footer1">2018 Ben Sanchez</p>
-          <br/>
+          <br />
           <p className="footer2">Connect With Me:</p>
           <div className="social">
-            <a href="https://www.linkedin.com/in/bnsnchz/"><img className="linkedIn" src="./assets/images/LinkedIn.png" alt="LinkedIn"/></a>
-            <a href="https://github.com/bnsnchz"><img className="gitHub" src="./assets/images/GitHub.png" alt="Github"/></a>
-            <a href="mailto:bnsnchzdev@gmail.com?Subject=Job%20Inquiry"><img className="mail" src="./assets/images/Mail.png" alt="mail" /></a>
+            <a href="https://www.linkedin.com/in/bnsnchz/"><img title="LinkedIn" className="linkedIn" src="./assets/images/LinkedIn.png" alt="LinkedIn" /></a>
+            <a href="https://github.com/bnsnchz"><img title="GitHub" className="gitHub" src="./assets/images/GitHub.png" alt="Github" /></a>
+            <a href="mailto:bnsnchzdev@gmail.com?Subject=Job%20Inquiry"><img title="E-Mail" className="mail" src="./assets/images/Mail.png" alt="mail" /></a>
+            <img title="Resume" className="resume" data-toggle="modal" data-target="#resume" src="./assets/images/Resume.png" alt="Resume" />
           </div>
         </footer>
       </div>
